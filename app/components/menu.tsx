62aecf36aa15f9eede107c562588c4f0edd4d7ec
rehanardianshahab <rehanardian1@gmail.com>
2025-09-09 01:04:56 +0700

    initial commit

---
+++ app/components/menu.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import React from "react";
+
+import { MenuIcon, ShipIcon, User2 } from "lucide-react";
+import { CgClose } from "react-icons/cg";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+import { GiNinjaMask } from "react-icons/gi";
+
+export default function Menu() {
+    const [isMenu, setMenu] = React.useState<boolean>(false);
+    const menuData = [
+        {icon: User2, label: "Profile", href: "/"},
+        {icon: GiNinjaMask, label: "Naruto", href: "/naruto"},
+        {icon: ShipIcon, label: "One Piece", href: "/onepiece"},
+    ]
+    return (
+        <div className="z-50 fixed right-12 bottom-12">
+            <div className="h-screen flex justify-center items-end w-full">
+                {/* Atur Menu saat muncul */}
+                <div className="fixed md:w-[calc(30vw-150px)] w-[calc(90vw-50px)] right-11 bottom-30 bg-red-900/90 backdrop-blur-[1px] text-white rounded-lg">
+                    <div className="flex flex-col items-end transition-all duration-300">
+                        {!isMenu || menuData.map((item, index) => (
+                            <MenuItem key={index} href={item.href} icon={item.icon} label={item.label} />
+                        ))}
+                    </div>
+                </div>
+                {/* Button Menu */}
+                <div className={cn("bg-red-900 rounded-full p-2 cursor-pointer hover:opacity-100 transition-all duration-300",
+                    isMenu ? "rotate-180" : "rotate-0 opacity-60"
+                )} onClick={() => setMenu(!isMenu)}>
+                    {!isMenu ? <MenuIcon size={36} className="text-white"/> : <CgClose size={36} className="text-white"/>}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function MenuItem({icon: Icon, label, href}: {icon: React.ElementType, label: string, href: string}) {
+    return (
+        <div className="p-4 border-b w-full text-right">
+            <Link href={href} className="flex justify-end items-center gap-2">
+                {Icon && <Icon size={20} />}
+                {label}
+            </Link>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,83 @@
+import { Globe, MapPinIcon } from "lucide-react";
 import Image from "next/image";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import Menu from "./components/menu";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <Menu />
+      <div className="center-container base-container">
+        <div className="w-full max-w-4xl shadow-2xl rounded-2xl overflow-hidden">
+          {/* Banner */}
+          <div className="bg-red-950 w-full h-62 sm:h-52 md:h-60 lg:h-64 rounded-t-2xl">
+            {/* Banner Teks */}
+            <div className="relative w-full h-full">
+              <div className="absolute flex justify-center w-full md:top-26 top-18">
+                <span className="text-white md:text-4xl text-2xl font-semibold">Frontend Developer</span>
+              </div>
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          {/* Content */}
+          <div className="p-6">
+            {/*  Pengatur Layout Vertikal */}
+            <div className="flex flex-col md:flex-row justify-between items-start md:items-end gap-4">
+              <div className="w-full space-y-4">
+                {/* Foto Profil */}
+                <div className="relative w-42 h-42 -mt-27 md:mx-0 mx-auto border-8 border-white rounded-full">
+                  <Image src="/profile.jpg" className="rounded-full" alt="Rehan Ardian Shahab" fill/>
+                </div>
+                
+                {/* Nama */}
+                <p className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-800">
+                  Rehan Ardian Shahab
+                </p>
+              </div>
+
+              {/* Info kanan */}
+              <div className="w-full flex flex-row md:items-center md:justify-end gap-3 md:gap-4">
+                <p className="flex items-center rounded-full gap-2 font-semibold bg-blue-800/10 text-blue-800 py-1.5 px-4">
+                  <MapPinIcon size={22} className="shrink-0" />
+                  <span className="inline-block text-sm sm:text-base">
+                    Bandung
+                  </span>
+                </p>
+
+              </div>
+
+            </div>
+            {/* Deskripsi */}
+            <div className="w-full text-justify space-y-4 mt-4">
+              <p><span className="text-2xl font-semibold"> Halo</span>, perkenalkan saya <b>Rehan Ardian Shahab</b>.
+              Saya seorang Frontend Developer yang fokus membangun antarmuka aplikasi dengan Next.js, Vue.js, dan Nuxt.js, serta memanfaatkan framework CSS seperti Tailwind CSS dan Windi CSS untuk membuat desain yang responsif dan modern.</p>
+              <p>Selain mengerjakan frontend, saya juga bisa menangani backend sederhana, seperti pembuatan API dan pengelolaan data menggunakan Sequelize dan Fastify.
+              Saya pernah berpengalaman sebagai Mentoring & Educational Consultant di PT Eduwork.id, sekaligus terlibat dalam proyek e-commerce sebagai frontend developer.</p>
+              <p>Saya selalu terbuka untuk belajar hal baru dan berusaha menghadirkan pengalaman pengguna yang lebih baik melalui setiap proyek yang saya kerjakan.</p>
+            </div>
+
+            {/* Social Media */}
+            <div className="mt-12">
+              <p className="font-bold mb-4">Ikuti saya di :</p>
+              <div className="flex md:justify-start justify-center gap-4 w-full">
+                <a href="https://www.linkedin.com/in/rehan-ardian-shahab-070097192/" className="text-blue-800 hover:text-blue-900 transition-colors duration-200" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin size={36} />
+                </a>
+                <a href="https://github.com/rehanardianshahab" className="hover:text-gray-700 transition-colors duration-200" target="_blank" rel="noopener noreferrer">
+                  <FaGithubSquare size={36} />
+                </a>
+                <a href="mailto:rehanardian1@gmail.com" className="hover:text-blue-700 text-blue-500 transition-colors duration-200" target="_blank" rel="noopener noreferrer">
+                  <MdEmail size={36} />
+                </a>
+                <a href="https://rehan-space.netlify.app/" className="hover:text-gray-700 text-gray-500 transition-colors duration-200" target="_blank" rel="noopener noreferrer">
+                  <Globe size={36} />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
